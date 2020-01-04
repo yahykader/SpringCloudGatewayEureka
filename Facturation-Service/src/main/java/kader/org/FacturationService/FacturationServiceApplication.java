46311f9c35e6ec30c3f417ed5e4c4b22257c4560
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2020-01-04 15:52:43 +0100

    add Facturation-service

---
+++ Facturation-Service/src/main/java/kader/org/FacturationService/FacturationServiceApplication.java
@@ -0,0 +1,13 @@
+package kader.org.FacturationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationServiceApplication.class, args);
+	}
+
+}
---
+++ Facturation-Service/src/test/java/kader/org/FacturationService/FacturationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kader.org.FacturationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
